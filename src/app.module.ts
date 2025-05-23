55b2c0cd76c080f2e68d55c5b204a94140c93c48
jwcbmat <jwilbruns@gmail.com>
2025-05-23 19:00:59 -0300

    setup: dotenv package added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DeliveriesController } from './deliveries/deliveries.controller';
+import { DeliveriesModule } from './deliveries/deliveries.module';
+import { ApiController } from './api/api.controller';
+import { ApiService } from './api/api.service';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), DeliveriesModule, ApiModule],
+  controllers: [AppController, DeliveriesController, ApiController],
+  providers: [AppService, ApiService],
 })
 export class AppModule {}
