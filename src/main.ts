3f527155b3e58ac672c0bde675a880899cb1a131
jwcbmat <jwilbruns@gmail.com>
2025-05-23 12:25:36 -0300

    setup: openAPI setup added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  SwaggerModule.setup('api', app, () =>
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('delivery fee wrapper')
+        .setDescription('description')
+        .setVersion('1.0')
+        .addTag('tags')
+        .build(),
+    ),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
