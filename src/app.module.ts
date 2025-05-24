de62dea00db1cc0eb0e22f98bcc773fc538625ef
jwcbmat <jwilbruns@gmail.com>
2025-05-24 20:13:13 -0300

    refactor: restructure base app files for new modular architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DeliveriesController } from './deliveries/deliveries.controller';
-import { DeliveriesModule } from './deliveries/deliveries.module';
-import { ApiController } from './api/api.controller';
-import { ApiService } from './api/api.service';
-import { ApiModule } from './api/api.module';
+import { ConfigModule } from './config/config.module';
+import { DeliveryModule } from './delivery/delivery.module';
+import { RequestsModule } from './requests/requests.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DeliveriesModule, ApiModule],
-  controllers: [AppController, DeliveriesController, ApiController],
-  providers: [AppService, ApiService],
+  imports: [ConfigModule, DeliveryModule, RequestsModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  SwaggerModule.setup('api', app, () =>
-    SwaggerModule.createDocument(
-      app,
-      new DocumentBuilder()
-        .setTitle('delivery fee wrapper')
-        .setDescription('description')
-        .setVersion('1.0')
-        .addTag('tags')
-        .build(),
-    ),
-  );
+  const config = new DocumentBuilder()
+    .setTitle('Minor Core API')
+    .setDescription('API for calculating delivery fee with margin')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-  await app.listen(process.env.PORT ?? 3000);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
