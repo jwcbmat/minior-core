9905055d356f6226af496a292624d2383da4c556
jwcbmat <jwilbruns@gmail.com>
2025-05-24 20:12:22 -0300

    chore: remove unused legacy modules 'api' and 'deliveries'

--- src/deliveries/deliveries.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DeliveriesService } from './deliveries.service';
-
-@Module({
-  providers: [DeliveriesService]
-})
-export class DeliveriesModule {}
