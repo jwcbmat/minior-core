a3b1723d43ea680d358ba4d96229e9beaf033707
jwcbmat <jwilbruns@gmail.com>
2025-05-25 20:07:37 -0300

    chore: package, pnpm-lock updated

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Minor Core API')
     .setDescription('API for calculating delivery fee with margin')
     .setVersion('1.0')
-    .addBearerAuth()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'auth-token',
+    )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
