037e7eea33f9292bc3f1b37bcf8fba61a36af46b
jwcbmat <jwilbruns@gmail.com>
2025-05-25 20:03:50 -0300

    refactor(delivery): update controller and delivery-response.dto to use new fee DTO

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -1,22 +1,47 @@
-import { Controller, Get, Query, Headers } from '@nestjs/common';
+import { UseGuards, Controller, Get, Query, Headers } from '@nestjs/common';
+import { DeliveryGuard } from './delivery.guard';
 import { DeliveryService } from './delivery.service';
+import { DeliveryQueryFeeDto } from './dto/delivery-query-fee.dto';
 import { DeliveryResponseDto } from './dto/delivery-response.dto';
 
+import {
+  ApiBearerAuth,
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+
+@ApiBearerAuth('auth-token')
+@ApiTags('delivery')
 @Controller('delivery')
 export class DeliveryController {
   constructor(private readonly deliveryService: DeliveryService) {}
 
+  @UseGuards(DeliveryGuard)
   @Get('fee')
+  @ApiOperation({
+    summary: 'Calculate delivery price',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Delivery fee successfully calculated',
+    type: DeliveryResponseDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid parameters. Check query values.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Access denied. Invalid or missing authentication token.',
+  })
   async getFee(
-    @Query('fromLat') fromLat: number,
-    @Query('fromLng') fromLng: number,
-    @Query('toLat') toLat: number,
-    @Query('toLng') toLng: number,
-    @Query('merchantId') merchantId: string,
-    @Query('uuid') uuid: string,
+    @Query() query: DeliveryQueryFeeDto,
     @Headers('authorization') authHeader: string,
     @Headers('user-agent') userAgent: string,
   ): Promise<DeliveryResponseDto> {
+    const { fromLat, fromLng, toLat, toLng, merchantId, uuid } = query;
+
     return this.deliveryService.calculateFee({
       from: { lat: fromLat, lng: fromLng },
       to: { lat: toLat, lng: toLng },
--- src/delivery/dto/delivery-response.dto.ts
+++ src/delivery/dto/delivery-response.dto.ts
@@ -1,14 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+class CoordinatesDto {
+  @ApiProperty({ example: 19.3331008 })
+  lat: number;
+
+  @ApiProperty({ example: -81.3801101 })
+  lng: number;
+}
+
+class CoordinatesWrapperDto {
+  @ApiProperty({ type: CoordinatesDto })
+  from: CoordinatesDto;
+
+  @ApiProperty({ type: CoordinatesDto })
+  to: CoordinatesDto;
+}
+
 export class DeliveryResponseDto {
+  @ApiProperty({ example: 8.13 })
   originalFee: number;
+
+  @ApiProperty({ example: 9.19 })
   newFee: number;
+
+  @ApiProperty({ example: 900 })
   deliveryTime: number;
+
+  @ApiProperty({ example: 6651.610000000001 })
   distanceMeters: number;
-  coordinates: {
-    from: { lat: number; lng: number };
-    to: { lat: number; lng: number };
-  };
+
+  @ApiProperty({ type: CoordinatesWrapperDto })
+  coordinates: CoordinatesWrapperDto;
+
+  @ApiProperty()
   uuid: string;
+
+  @ApiProperty()
   merchantId: string;
+
+  @ApiProperty()
   userAgent: string;
+
+  @ApiProperty({ nullable: true, example: 'happy message :)' })
   message: string | null;
 }
