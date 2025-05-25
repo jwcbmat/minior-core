aaff021603ea11a5af41580ea1f93edb5f02adc5
jwcbmat <jwilbruns@gmail.com>
2025-05-25 20:04:54 -0300

    refactor(requests): improve requests controller and stored-requests-response.dto

--- src/requests/dto/stored-request-response.dto.ts
+++ src/requests/dto/stored-request-response.dto.ts
@@ -1,23 +1,98 @@
-export class StoredRequestResponseDto {
-  originalFee: number;
-  newFee: number;
-  deliveryTime: number;
-  distanceMeters: number;
-  message: string | null;
-  timestamp: string;
-  userAgent: string;
+import {
+  IsNumber,
+  IsString,
+  IsObject,
+  ValidateNested,
+  IsOptional,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+import { ApiProperty } from '@nestjs/swagger';
+
+class Coordinates {
+  @IsNumber()
+  lat: number;
+
+  @IsNumber()
+  lng: number;
+}
+
+class CoordinatesWrapper {
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Coordinates)
+  from: Coordinates;
+
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Coordinates)
+  to: Coordinates;
 }
 
 export class SavedRequestDto {
+  @IsNumber()
   originalFee: number;
+
+  @IsNumber()
   newFee: number;
+
+  @IsNumber()
   deliveryTime: number;
+
+  @IsNumber()
   distanceMeters: number;
+
+  @IsObject()
+  @ValidateNested()
+  @Type(() => CoordinatesWrapper)
+  coordinates: CoordinatesWrapper;
+
+  @IsString()
+  uuid: string;
+
+  @IsString()
+  merchantId: string;
+
+  @IsString()
+  userAgent: string;
+}
+
+export class StoredRequestResponseDto {
+  @ApiProperty({ example: 8.13 })
+  originalFee: number;
+
+  @ApiProperty({ example: 9.19 })
+  newFee: number;
+
+  @ApiProperty({ example: 900 })
+  deliveryTime: number;
+
+  @ApiProperty({ example: 6651.61 })
+  distanceMeters: number;
+
+  @ApiProperty({ example: 'uuid-1234' })
+  uuid: string;
+
+  @ApiProperty({ example: 'merchant-123' })
+  merchantId: string;
+
+  @ApiProperty()
+  userAgent: string;
+
+  @ApiProperty({ example: '2025-05-25T15:30:00Z' })
+  timestamp: string;
+
+  @ApiProperty({
+    example: {
+      from: { lat: 19.3331008, lng: -81.3801101 },
+      to: { lat: 19.280354, lng: -81.373868 },
+    },
+  })
   coordinates: {
     from: { lat: number; lng: number };
     to: { lat: number; lng: number };
   };
-  uuid: string;
-  merchantId: string;
-  userAgent: string;
+
+  @ApiProperty({ nullable: true, example: 'happy message :)' })
+  message: string | null;
 }
--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { StoredRequestResponseDto } from './dto/stored-request-response.dto';
 
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+@ApiTags('requests')
 @Controller('requests')
 export class RequestsController {
   constructor(private readonly requestsService: RequestsService) {}
 
   @Get('last')
+  @ApiOperation({
+    summary: 'Get the 10 most recent delivery fee requests',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved the 10 most recent delivery requests.',
+    type: [StoredRequestResponseDto],
+  })
+  @ApiResponse({
+    status: 400,
+    description:
+      'Bad request. Could not retrieve the recent delivery requests.',
+  })
   async getLastRequests(): Promise<StoredRequestResponseDto[]> {
-    return this.requestsService.getLastRequests();
+    return await this.requestsService.getLastRequests();
   }
 }
