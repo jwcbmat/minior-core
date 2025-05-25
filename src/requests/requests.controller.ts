eeab8a07160d5fabc07d20d17c9ecb3b12d0d3dc
jwcbmat <jwilbruns@gmail.com>
2025-05-25 20:16:24 -0300

    chore: remove unused InternalServerErrorExceptionl

--- src/requests/requests.controller.ts
+++ src/requests/requests.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { StoredRequestResponseDto } from './dto/stored-request-response.dto';
 
